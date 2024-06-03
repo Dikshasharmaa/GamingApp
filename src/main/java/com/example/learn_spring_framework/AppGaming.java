305d30dc6736c9cf0305147de7f5b879bd9104c6
Dikshasharmaa <diksha66sharma78@gmail.com>
2024-06-03 17:31:17 -0400

    Made a new class SuperContra and leraned about tight coupling

--- src/main/java/com/example/learn_spring_framework/AppGaming.java
+++ src/main/java/com/example/learn_spring_framework/AppGaming.java
@@ -2,13 +2,14 @@ package com.example.learn_spring_framework;
 
 import com.example.learn_spring_framework.game.GameRunner;
 import com.example.learn_spring_framework.game.MarioGame;
+import com.example.learn_spring_framework.game.SuperContraGame;
 
 public class AppGaming {
 
 	public static void main(String[] args) {
-		
-		var marioGame = new MarioGame();
-		var gameRunner = new GameRunner(marioGame);
+//		var MarioGame = new MarioGame();
+		var SuperContraGame = new SuperContraGame();
+		var gameRunner = new GameRunner(SuperContraGame);
 		gameRunner.run();
 	}
 
--- src/main/java/com/example/learn_spring_framework/game/GameRunner.java
+++ src/main/java/com/example/learn_spring_framework/game/GameRunner.java
@@ -1,18 +1,18 @@
 package com.example.learn_spring_framework.game;
 
 public class GameRunner {
-	MarioGame game;
-	
-	public GameRunner(MarioGame game) {
-		this.game = game;
+//	MarioGame game;
+	SuperContraGame game1;
+	public GameRunner(SuperContraGame game1) {
+		this.game1 = game1;
 	}
 
 	public void run() {
-		System.out.println("Running game"+ game);
-		game.up();
-		game.down();
-		game.left();
-		game.right();
+		System.out.println("Running game"+ game1);
+		game1.up();
+		game1.down();
+		game1.left();
+		game1.right();
 	}
 
 }
